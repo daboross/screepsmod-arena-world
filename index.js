cf0eeeea14017523b3f9a3c23785513cd4e9734f
Dabo Ross <daboross@daboross.net>
2016-11-16 14:11:48 -0800

    Update mod for latest server version

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const Arena = require('./lib/arena');
 
 module.exports = function (config) {
     if (config.cli) {
-        var oldCreateSandbox = config.cli.createSandbox;
-        config.cli.createSandbox = function() {
-            var sandbox = oldCreateSandbox.apply(this, Array.prototype.slice.call(arguments));
+        config.cli.on('cliSandbox', function (sandbox) {
             sandbox.arena = new Arena(sandbox.map);
-            return sandbox;
         }
     }
 }
